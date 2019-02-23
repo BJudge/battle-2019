075733c8a6a1ff597c100bf55c81e904260deb47
BartJudge <BartJudge@gmail.com>
2019-02-23 15:15:59 +0000

    tests now passing for implementing hit points

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
   DEFAULT_HIT_POINTS = 60
-  attr_reader :name, :hit_points
+  attr_accessor :name, :hit_points
 
   def initialize(name, hit_points = DEFAULT_HIT_POINTS)
     @name = name
