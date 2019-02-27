0396d0e03aa0e54a82fdfc28aef9b07d53a7320b
BartJudge <BartJudge@gmail.com>
2019-02-27 03:35:02 +0000

    removing $player for $game

--- app.rb
+++ app.rb
@@ -1,30 +1,34 @@
 require 'sinatra'
 require_relative './lib/player.rb'
+require_relative './lib/game.rb'
 
 class Battle < Sinatra::Base
-
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_2_hit_points = $player_2.hit_points
+    @game = $game
+    @player_1_name = @game.player_1.name
+    @player_2_name = @game.player_2.name
+    @player_2_hit_points = @game.player_2.hit_points
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_1_attack = Game.new.attack($player_2)
+    @game = $game
+    @player_1_name = @game.player_1.name
+    @player_2_name = @game.player_2.name
+    @player_1_attack = @game.attack(@game.player_2)
     erb :attack
   end
 
